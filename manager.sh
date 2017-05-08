82d577e9402468dc9bb47ce4f2b9e79d3f07dd5c
zhangshuang <shuang199054@163.com>
2017-05-08 13:58:33 +0800

    filebeat docker

--- manager.sh
+++ manager.sh
@@ -6,26 +6,21 @@ devops_prj_path="$prj_path/devops"
 template_path="$devops_prj_path/template"   
 config_path="$devops_prj_path/config"   
 
-syslogng_image=docker.sunfund.com/balabit/syslog-ng
-syslogng_container=syslog-ng
+filebeat_image=prima/filebeat
+filebeat_container=filebeat
 
 source $devops_prj_path/base.sh
 
-log_path='/opt/data/syslog-ng'
+log_path='/opt/data/filebeat'
 
 function run() {
     local args='--restart=always'
-    args="$args --net=host"
-    args="$args -v $log_path:/var/log"
-    # we use host network
-    # so this is usless
-    # args="$args -p 514:514/udp -p 601:601/tcp"
-    args="$args -v $config_path/syslog-ng.conf:/etc/syslog-ng/conf.d/syslog-ng.conf"
-    run_cmd "docker run -d $args --name $syslogng_container $syslogng_image"
+    args="$args -v $config_path/filebeat.yml:/filebeat.yml"
+    run_cmd "docker run -d $args --name $filebeat_container $filebeat_image"
 }
 
 function stop() {
-    stop_container $syslogng_container
+    stop_container $filebeat_container
 }
 
 function restart() {
